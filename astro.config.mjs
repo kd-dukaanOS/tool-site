cd4efe89e68828ab370821e84180325c4cb2ea65
kd-dukaanOS <kunalkubazu@gmail.com>
2026-08-20 16:56:46 +0530

    Add robotsfavicon sitexml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://tool-site.kunalkubazu.workers.dev",
+  site: "https://toomanytoolz.com",
 
   integrations: [sitemap()]
 });
