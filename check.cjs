0e11c19fca374d0ce1c1fd54423a1e8c204602f3
kd-dukaanOS <kunalkubazu@gmail.com>
2026-08-27 19:56:11 +0530

    Fix data files

---
+++ check.cjs
@@ -0,0 +1,11 @@
+﻿const fs = require('fs');
+const path = require('path');
+const dir = 'src/data/calculators';
+const raw = fs.readFileSync('src/data/calculator-content-map.js', 'utf8');
+const pairs = [...raw.matchAll(/["']([\w-]+)["']\s*:\s*["']([\w-]+)["']/g)];
+const missing = [];
+for (const [, slug, file] of pairs) {
+  if (!fs.existsSync(path.join(dir, file + '.ts'))) missing.push(slug + ' -> ' + file + '.ts');
+}
+console.log('Checked', pairs.length, 'entries,', missing.length, 'missing:');
+missing.forEach(m => console.log(' ', m));
